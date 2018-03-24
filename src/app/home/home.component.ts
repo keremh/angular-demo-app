0c8a0c674de465eca434e93e8c322f4c10662976
keremh <hallackerem@gmail.com>
2018-03-24 23:07:02 +0300

    animations feature added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,14 +4,36 @@ import { trigger, style, transition, animate, keyframes, query, stagger } from '
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+
+    trigger('goals', [
+      transition('* => *', [
+        query(':enter', style({opacity: 0}), {optional: true}),
+
+        query(':enter', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+            style({opacity: 1, transform: 'translateY(0)', offset: 1}),
+          ]))]), {optional: true}),
+
+        query(':leave', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 1}),
+          ]))]), {optional: true})
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
   itemCount: number;
   btnText: string = 'Add an item';
   goalText: string = 'My first life goal';
-  goals = [];
+  goals = ['My first life goal', 'I want to climb a mountain', 'Go ice skiing'];
   constructor() { }
 
   ngOnInit() {
@@ -22,5 +44,9 @@ export class HomeComponent implements OnInit {
     this.goals.push(this.goalText);
     this.goalText = '';
     this.itemCount = this.goals.length;
+  }
+
+  removeItem(i){
+    this.goals.splice(i, 1);
   }
 }
